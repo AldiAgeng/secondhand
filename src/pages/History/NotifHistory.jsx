3f92782443ff0a2dfd9679438c282338bc6d7285
AldiAgeng <aldiageng48@gmail.com>
2022-07-22 17:53:19 +0700

    fix bug notif, and update notif is_read

--- src/pages/History/NotifHistory.jsx
+++ src/pages/History/NotifHistory.jsx
@@ -28,43 +28,14 @@ function NotifHistory({ users }) {
   useEffect(() => {
     getNotif();
   }, []);
-  useEffect(() => {
-    {
-      sellers.forEach((data) => {
-        console.log(data, "data effect");
-        if (data.is_read === false) {
-          axios
-            .patch(
-              `https://tokoku-api-2.herokuapp.com/api/v1/notification/${data.id}`,
-              {
-                headers: {
-                  Authorization: "Bearer " + localStorage.getItem("token"),
-                  "Content-Type": "application/json",
-                },
-              }
-            )
-            .then((response) => {
-              console.log(response, "udah dilooping");
-            })
-            .catch((err) => {
-              console.log(localStorage.getItem("token"), "token");
-              console.log(err, "err");
-            });
-        }
-      });
-    }
-  }, []);
+
   return (
     <>
       <NavbarMenu />
       <BackButton />
       <Container className="py-5 mt-5">
         <div className="boxUserOrder">
-          <img
-            src={users.picture}
-            alt={users.picture}
-            className="imgUserOrder"
-          />
+          <img src={users.picture} alt={users.picture} className="imgUserOrder" />
           <div>
             <h5 className="textContent">{users.name}</h5>
             <p className="textGray">{users.city}</p>
@@ -77,61 +48,68 @@ function NotifHistory({ users }) {
           <Col md={6} className="mb-3">
             <h3 className="textTitle">Notifikasi Seller</h3>
             <div className="scrollContent">
-              {/*  <div className="productOrder">
-                <div className="d-flex flex-row">
-                  <div>
-                    <img
-                      className="imgProductOrder"
-                      src={data.Order.Product.picture}
-                      alt={data.Order.Product.picture}
-                    />
-                  </div>
-                  <div>
-                    <p className="textGray">Penawaran Produk Baru</p>
-                    <h5 className="textContent">{data.Order.Product.name}</h5>
-                    <h5 className="textContent">
-                      Rp {data.Order.Product.price.toLocaleString("id-ID")}
-                    </h5>
-                  </div>
-                </div>
-                <div className="text-end">
-                  <p className="textGray mb-2">
-                    {moment(data.createdAt).format("L")}
-                  </p>
-                  <Link
-                    style={{
-                      textDecoration: "none",
-                      color: "black",
-                    }}
-                    to={`/info-penawar/${data.id_order}`}
-                  >
-                    <BtnPrimary>Detail</BtnPrimary>
-                  </Link>
-                </div>
-              </div> */}
+              {sellers.map((data) => {
+                if (data.Order !== null) {
+                  if (data.is_read === false && data.Order.Product.id_user === users.id) {
+                    axios
+                      .patch(`https://tokoku-api-2.herokuapp.com/api/v1/notification/${data.id}`, null, {
+                        headers: {
+                          Authorization: "Bearer " + localStorage.getItem("token"),
+                        },
+                      })
+                      .then((response) => {
+                        console.log(response.data, "responsenya seller cuy");
+                      })
+                      .catch((err) => {
+                        console.log(err, "err seller");
+                      });
+                  }
+                  return (
+                    <div className="productOrder">
+                      <div className="d-flex flex-row">
+                        <div>
+                          <img className="imgProductOrder" src={data.Order.Product.picture} alt={data.Order.Product.picture} />
+                        </div>
+                        <div>
+                          <p className="textGray">Penawaran Produk Baru</p>
+                          <h5 className="textContent">{data.Order.Product.name}</h5>
+                          <h5 className="textContent">Rp {data.Order.Product.price.toLocaleString("id-ID")}</h5>
+                        </div>
+                      </div>
+                      <div className="text-end">
+                        <p className="textGray mb-2">{moment(data.createdAt).format("L")}</p>
+                        <Link
+                          style={{
+                            textDecoration: "none",
+                            color: "black",
+                          }}
+                          to={`/info-penawar/${data.id_order}`}
+                        >
+                          <BtnPrimary>Detail</BtnPrimary>
+                        </Link>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
             </div>
           </Col>
           <Col md={6}>
             <h3 className="textTitle">Notifikasi Buyer</h3>
             <div className="scrollContent">
               {buyers.map((data) => {
-                if (data.is_read === false) {
+                if (data.is_read === false && data.Order.id_user === users.id) {
                   axios
-                    .patch(
-                      `https://tokoku-api-2.herokuapp.com/api/v1/notification/${data.id}`,
-                      {
-                        headers: {
-                          Authorization:
-                            "Bearer " + localStorage.getItem("token"),
-                          accept: "application/json",
-                        },
-                      }
-                    )
+                    .patch(`https://tokoku-api-2.herokuapp.com/api/v1/notification/${data.id}`, null, {
+                      headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token"),
+                      },
+                    })
                     .then((response) => {
-                      console.log(response, "udah dilooping");
+                      console.log(response, "responsenya buyer cuy");
                     })
                     .catch((err) => {
-                      console.log(err, "err");
+                      console.log(err, "err buyer");
                     });
                 }
 
@@ -141,32 +119,16 @@ function NotifHistory({ users }) {
                       <div className="productOrder" key={data.id}>
                         <div className="d-flex flex-row">
                           <div>
-                            <img
-                              className="imgProductOrder"
-                              src={data.Order.Product.picture}
-                              alt={data.Order.Product.picture}
-                            />
+                            <img className="imgProductOrder" src={data.Order.Product.picture} alt={data.Order.Product.picture} />
                           </div>
                           <div>
-                            <p className="textGray">
-                              Anda berhasil melakukan penawaran
-                            </p>
-                            <h5 className="textContent">
-                              {data.Order.Product.name}
-                            </h5>
-                            <h5 className="textContent">
-                              Rp{" "}
-                              {data.Order.Product.price.toLocaleString("id-ID")}
-                            </h5>
-                            <h5 className="textContent">
-                              Ditawar Rp{" "}
-                              {data.Order.price.toLocaleString("id-ID")}
-                            </h5>
+                            <p className="textGray">Anda berhasil melakukan penawaran</p>
+                            <h5 className="textContent">{data.Order.Product.name}</h5>
+                            <h5 className="textContent">Rp {data.Order.Product.price.toLocaleString("id-ID")}</h5>
+                            <h5 className="textContent">Ditawar Rp {data.Order.price.toLocaleString("id-ID")}</h5>
                           </div>
                           <div className="text-end">
-                            <p className="textGray mb-2">
-                              {moment(data.createdAt).format("L")}
-                            </p>
+                            <p className="textGray mb-2">{moment(data.createdAt).format("L")}</p>
                           </div>
                         </div>
                       </div>
@@ -176,73 +138,35 @@ function NotifHistory({ users }) {
                           <div className="productOrder" key={data.id}>
                             <div className="d-flex flex-row">
                               <div>
-                                <img
-                                  className="imgProductOrder"
-                                  src={data.Order.Product.picture}
-                                  alt={data.Order.Product.picture}
-                                />
+                                <img className="imgProductOrder" src={data.Order.Product.picture} alt={data.Order.Product.picture} />
                               </div>
                               <div>
-                                <p className="textGray">
-                                  Penawaran anda disetujui
-                                </p>
-                                <h5 className="textContent">
-                                  {data.Order.Product.name}
-                                </h5>
-                                <h5 className="textContent">
-                                  Rp{" "}
-                                  {data.Order.Product.price.toLocaleString(
-                                    "id-ID"
-                                  )}
-                                </h5>
-                                <h5 className="textContent">
-                                  Ditawar Rp{" "}
-                                  {data.Order.price.toLocaleString("id-ID")}
-                                </h5>
+                                <p className="textGray">Penawaran anda disetujui</p>
+                                <h5 className="textContent">{data.Order.Product.name}</h5>
+                                <h5 className="textContent">Rp {data.Order.Product.price.toLocaleString("id-ID")}</h5>
+                                <h5 className="textContent">Ditawar Rp {data.Order.price.toLocaleString("id-ID")}</h5>
                               </div>
                             </div>
                             <div className="text-end">
-                              <p className="textGray mb-2">
-                                {moment(data.createdAt).format("L")}
-                              </p>
+                              <p className="textGray mb-2">{moment(data.createdAt).format("L")}</p>
                             </div>
                           </div>
                         ) : (
                           <div className="productOrder" key={data.id}>
                             <div className="d-flex flex-row">
                               <div>
-                                <img
-                                  className="imgProductOrder"
-                                  src={data.Order.Product.picture}
-                                  alt={data.Order.Product.picture}
-                                />
+                                <img className="imgProductOrder" src={data.Order.Product.picture} alt={data.Order.Product.picture} />
                               </div>
                               <div>
-                                <p className="textGray">
-                                  Penawaran anda ditolak
-                                </p>
-                                <h5 className="textContent">
-                                  {data.Order.Product.name}
-                                </h5>
-                                <h5 className="textContent">
-                                  Rp{" "}
-                                  {data.Order.Product.price.toLocaleString(
-                                    "id-ID"
-                                  )}
-                                </h5>
-                                <h5 className="textContent">
-                                  Ditawar Rp{" "}
-                                  {data.Order.price.toLocaleString("id-ID")}
-                                </h5>
-                                <p className="textGray">
-                                  cek history order untuk merubah penawaran.
-                                </p>
+                                <p className="textGray">Penawaran anda ditolak</p>
+                                <h5 className="textContent">{data.Order.Product.name}</h5>
+                                <h5 className="textContent">Rp {data.Order.Product.price.toLocaleString("id-ID")}</h5>
+                                <h5 className="textContent">Ditawar Rp {data.Order.price.toLocaleString("id-ID")}</h5>
+                                <p className="textGray">cek history order untuk merubah penawaran.</p>
                               </div>
                             </div>
                             <div className="text-end">
-                              <p className="textGray mb-2">
-                                {moment(data.createdAt).format("L")}
-                              </p>
+                              <p className="textGray mb-2">{moment(data.createdAt).format("L")}</p>
                             </div>
                           </div>
                         )}
